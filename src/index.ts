6a6ab734623ea1905a8a6db9cc2f37ff0befd8e1
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-02-07 11:13:31 -0700

    update schema file, codegen, and tsconfig, along with types file for graphql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
 import { resolvers } from "./resolvers";
 import { SpotifyAPI } from "./datasources/spotify-api";
+
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
 
 async function startApolloServer() {
   const server = new ApolloServer({
