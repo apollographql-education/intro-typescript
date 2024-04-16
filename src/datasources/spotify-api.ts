527f60f44c1e08eb5f4359b655ca72d378a69547
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-04-15 20:45:10 -0600

    update final code

--- src/datasources/spotify-api.ts
+++ src/datasources/spotify-api.ts
@@ -1,12 +1,18 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import { Playlist } from "../types"
 import { PlaylistModel, SnapshotOrError } from '../models'
 
 export class SpotifyAPI extends RESTDataSource {
   baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
-  
+
   async getFeaturedPlaylists(): Promise<PlaylistModel[]> {
-    const { playlists: { items } } : { playlists: { items: [] }} = await this.get("browse/featured-playlists");
-    return items;
+    const response = await this.get<{
+      playlists: {
+        items: PlaylistModel[];
+      };
+    }>("browse/featured-playlists");
+
+    return response?.playlists?.items ?? [];
   }
 
   getPlaylist(playlistId: string): Promise<PlaylistModel> {
