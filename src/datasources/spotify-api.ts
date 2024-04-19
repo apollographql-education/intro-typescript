63ddbc6c44327eccadbdcb8ad8ed52769bdc55a7
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-04-19 12:05:32 -0600

    update resolvers and add method to spotify api

--- src/datasources/spotify-api.ts
+++ src/datasources/spotify-api.ts
@@ -1,6 +1,6 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 import { Playlist } from "../types"
-import { PlaylistModel, SnapshotOrError } from '../models'
+import { PlaylistModel, TrackModel, SnapshotOrError } from '../models'
 
 export class SpotifyAPI extends RESTDataSource {
   baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
@@ -13,6 +13,12 @@ export class SpotifyAPI extends RESTDataSource {
     }>("browse/featured-playlists");
 
     return response?.playlists?.items ?? [];
+    
+  }
+
+  async getTracks(playlistId: string): Promise<TrackModel[]> {
+    const response = await this.get<{ items: { track: TrackModel }[] }>(`playlists/${playlistId}/tracks`)
+    return response?.items?.map(({track}) => track) ?? [];
   }
 
   getPlaylist(playlistId: string): Promise<PlaylistModel> {
